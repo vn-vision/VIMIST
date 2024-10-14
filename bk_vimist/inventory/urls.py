a66f168ebd66495706db84de4e5ab4502d6b9dc1
kZilla <kzilla@vn-z.zilla>
2024-10-15 00:00:00 +0300

    Reports and analytics on Inventory: Move to Sales

--- bk_vimist/inventory/urls.py
+++ bk_vimist/inventory/urls.py
@@ -8,9 +8,15 @@ This setup will automatically create the following endpoints:
 """
 from rest_framework.routers import DefaultRouter
 from inventory.views import ProductViewSet
+from django.urls import path
 
 # define the api end points
 router = DefaultRouter()  # automatically generates url patterns for ViewSets
 router.register(r'products', ProductViewSet)  # register urls with prefix products
 
 urlpatterns = router.urls
+urlpatterns += [
+    path('stock_levels/', ProductViewSet.as_view({'get': 'stock_levels'}), name='stock_levels'),
+    path('inventory_valuation/', ProductViewSet.as_view({'get': 'inventory_valuation'}), name='inventory_valuation'),
+    path('inventory_trend/', ProductViewSet.as_view({'get': 'inventory_trend'}), name='inventory_trend'),
+]
