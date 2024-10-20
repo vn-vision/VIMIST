4080f9a2266b045d851c26844a59d36a7ddaa513
kZilla <kzilla@vn-z.zilla>
2024-10-21 00:00:00 +0300

    Roles Roles Roles

--- bk_vimist/bk_vimist/settings.py
+++ bk_vimist/bk_vimist/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', 
+        'rest_framework.permissions.IsAuthenticated', 
     ],
 
     # configure pagination
