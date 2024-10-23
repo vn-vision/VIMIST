ee1d6bead159616d64baaea670b023f68f9b080d
vn-vision <vctrngang@gmail.com>
2024-10-23 13:39:06 +0300

    authentication first ..he he he

---
+++ fr-vimist/src/features/authentication/authSlice.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const authSlice = () => {
+  return (
+  )
+}
+
+export default authSlice
--- fr-vimist/src/store/store.ts
+++ fr-vimist/src/store/store.ts
@@ -6,10 +6,11 @@ import paymentsSlice from "../features/payments/paymentsSlice";
 import notificationsSlice from "../features/notifications/notificationsSlice";
 import creditSlice from "../features/creditsales/creditSlice";
 import customersSlice from "../features/customers/customersSlice";
-
+import authSlice from "../features/authentication/authSlice";
 
 const store = configureStore({
     reducer: {
+        auth:authSlice,
         inventory: inventorySlice,
         purchases: purchasesSlice,
         sales: salesSlice,
